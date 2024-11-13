bd8838f70aa0afdcbb148f35a5d59f4303d63eee
Mohammed Ali Samhouri <mohammedalisamhouri@gmail.com>
2024-11-13 19:24:46 +0300

    formatting: clearing the code

--- app/src/main/java/com/example/schoolapp/Presentation/Screens/Classes.kt
+++ app/src/main/java/com/example/schoolapp/Presentation/Screens/Classes.kt
@@ -3,104 +3,123 @@ package com.example.schoolapp.Presentation.Screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose.AppTheme
-import com.example.schoolapp.Data.MockData.Mock
 import com.example.schoolapp.Data.MockData.Mock.classList
 import com.example.schoolapp.Data.MockData.Mock.daysOfWeek
 import com.example.schoolapp.Presentation.Screens.ScreensPieces.ClassesTopAppBar
 import com.example.schoolapp.Presentation.VM.MainViewModel
 
+//=======================================================
+//sessions table: Logic & UI                            =
+//=======================================================
+//todo @LT #simple || @(37:69)=="MainViewModel" variable name must start with small litter
+//todo @LT #medium~#hard || try adding the @preview notation to be able to use the design tab
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StudentClass(MainViewModel: MainViewModel = MainViewModel()) {
+
+    //todo @MAS #simple || please add the usage after answering the referred todo task
     LaunchedEffect(Unit) {
         MainViewModel.isTopappbarVisible5()
-
     }
 
+    //=======================================================
+    //variables: local & stats                              =
+    //=======================================================
     val state by MainViewModel.Classesstate.collectAsState()
 
+    //=======================================================
+    // Logic & UI                                           =
+    //=======================================================
     AppTheme {
         Surface(
             modifier = Modifier
-                .fillMaxSize(), color = MaterialTheme.colorScheme.primaryContainer
+                .fillMaxSize(),
+                color = MaterialTheme.colorScheme.primaryContainer
         ) {
             Scaffold(
                 containerColor = MaterialTheme.colorScheme.onPrimary,
+                //todo @MAS #simple || please add the usage after answering the referred todo task
                 topBar = {
-
-                    ClassesTopAppBar(viewModel =MainViewModel , modifier =Modifier , Title ="Classes")
+                    ClassesTopAppBar(
+                        viewModel = MainViewModel,
+                        modifier = Modifier,
+                        Title = "Classes"
+                    )
                 },
                 // Add content padding
             ) {
+                //Main page UI: column
                 Column(
                     modifier = Modifier
                         .padding(it)
                         .fillMaxSize()
                 ) {
+                    //Main column
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(16.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        //Main lazy column
                         LazyColumn(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .padding(10.dp)
                         ) {
+                            //todo @LT #qustion[not answered] || is it working on sequence 0,1,2,,,,,n?
                             items(daysOfWeek.size) {
+                                //child column of the lazyColumn
                                 Column {
-
+                                    //represent the day name
                                     Text(
                                         text = daysOfWeek[it],
                                         fontSize = 30.sp,
                                         color = MaterialTheme.colorScheme.secondary
                                     )
+
+                                    //child lazy row
                                     LazyRow {
+                                        //todo @MAS #simple || please add the usage after answering the referred todo task
                                         items(classList.size) {
+
+                                            //Card UI to hold the session data
                                             Card(
                                                 modifier = Modifier
                                                     .padding(16.dp)
                                                     .size(150.dp),
                                                 colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
                                             ) {
-                                                Column(modifier = Modifier
-                                                    .fillMaxSize()
-                                                    .padding(16.dp),
+                                                //todo @LT #meduim || please consider changing the design here with @MAS
+                                                Column(
+                                                    modifier = Modifier
+                                                        .fillMaxSize()
+                                                        .padding(16.dp),
                                                     horizontalAlignment = Alignment.CenterHorizontally,
                                                     verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
-                                                    ) {
+                                                ) {
                                                     Text(
                                                         text = classList[it].subjectName,
                                                         fontSize = 20.sp,
@@ -108,7 +127,7 @@ fun StudentClass(MainViewModel: MainViewModel = MainViewModel()) {
                                                     )
                                                     Spacer(modifier = Modifier.height(10.dp))
                                                     Text(
-                                                        text =classList[it].teacher,
+                                                        text = classList[it].teacher,
                                                         fontSize = 20.sp,
                                                         color = MaterialTheme.colorScheme.secondary
                                                     )
@@ -118,36 +137,21 @@ fun StudentClass(MainViewModel: MainViewModel = MainViewModel()) {
                                                         fontSize = 20.sp,
                                                         color = MaterialTheme.colorScheme.secondary
                                                     )
-
                                                 }
-
-
-
-
                                             }
                                         }
-
                                     }
                                 }
-
-
                             }
                         }
-
-
                     }
-
                 }
-
             }
-
-
         }
-
-
     }
 }
 
+//todo @LT #simple || put this in the relative file
 @Composable
 @Preview
 fun StudentClassPreview() {
