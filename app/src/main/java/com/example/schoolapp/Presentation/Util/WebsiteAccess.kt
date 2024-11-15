75a656102f3307d895c823c272f83c1070d2c976
Mohammed Ali Samhouri <mohammedalisamhouri@gmail.com>
2024-11-15 16:53:52 +0300

    formatting: clearing the code

--- app/src/main/java/com/example/schoolapp/Presentation/Util/WebsiteAccess.kt
+++ app/src/main/java/com/example/schoolapp/Presentation/Util/WebsiteAccess.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 
+//used to open the given URL website
 fun openWebsite(context: Context, url: String) {
     val customTabsIntent = CustomTabsIntent.Builder().build()
     customTabsIntent.launchUrl(context, Uri.parse(url))
