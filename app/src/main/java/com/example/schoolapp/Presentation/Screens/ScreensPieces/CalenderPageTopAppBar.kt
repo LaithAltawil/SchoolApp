99b4cd5779dcdfd1885a43a09c6f4be41c94a895
Laith Altawil <tdmer2003@gmail.com>
2024-11-15 17:11:38 +0300

    done with all todos

--- app/src/main/java/com/example/schoolapp/Presentation/Screens/ScreensPieces/CalenderPageTopAppBar.kt
+++ app/src/main/java/com/example/schoolapp/Presentation/Screens/ScreensPieces/CalenderPageTopAppBar.kt
@@ -1,10 +1,5 @@
 package com.example.schoolapp.Presentation.Screens.ScreensPieces
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,8 +15,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,14 +23,14 @@ import androidx.compose.ui.unit.sp
 import com.example.schoolapp.Presentation.VM.MainViewModel
 
 //=======================================================
-//todo @LT #simple || explain this fun logic here       =
+//solved @LT #simple || explain this fun logic here       =
 //I will Be removing the animation parts(@44:54) these files(________TOPAPPBAR.KT) since such animations are really not needed
 // this is the designs of top app bars where each file contain almost the
 // same design except the title and some changes
 //=======================================================
-//todo @LT #simple || @(37:69)=="Title" variable name must start with small litter
+//solved @LT #simple || @(37:69)=="Title" variable name must start with small litter
 //done
-//todo @LT #medium~#hard || try adding the @preview notation to be able to use the design tab
+//solved @LT #medium~#hard || try adding the @preview notation to be able to use the design tab
 //Done
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
